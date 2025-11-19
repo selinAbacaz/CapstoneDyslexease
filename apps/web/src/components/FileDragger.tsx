b1ac396b970e089941fbf6ba45e822299682d8e5
TheLuckyman30 <bnauta0@gmail.com>
2025-11-19 10:19:20 -0500

    New lines will no longer be automatically added when a file is read

--- apps/web/src/components/FileDragger.tsx
+++ apps/web/src/components/FileDragger.tsx
@@ -28,16 +28,12 @@ export function DropFile(props: DropFilesProps) {
     const userFiles = Array.from(event.dataTransfer.files);
     props.setFile(userFiles);
 
-    let accumWords: string = '';
-
     userFiles.forEach((file: File) => {
       const reader = new FileReader();
 
       reader.onloadend = () => {
         if (typeof reader.result === 'string') {
-          const removedLineBreaks = reader.result.replace(/[\r\n]+/gm, ' ');
-          accumWords += removedLineBreaks.split('.').join('.\n');
-          props.setWords(accumWords);
+          props.setWords(reader.result);
         }
       };
 
@@ -68,8 +64,8 @@ export function DropFile(props: DropFilesProps) {
           border: '1px dotted',
           backgroundColor: props.isDragOver ? 'lightgray' : 'white',
           color: 'black',
-          fontFamily: props.fontFamily, 
-          boxShadow: '2px 2px 10px #99aee7'
+          fontFamily: props.fontFamily,
+          boxShadow: '2px 2px 10px #99aee7',
         }}
       >
         Drag files
