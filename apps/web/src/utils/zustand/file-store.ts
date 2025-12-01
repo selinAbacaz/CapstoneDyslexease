e9f4d0dffbe84ca7f03b0afe885a2a5f47c4ed7b
Listowel <animlistowel@gmail.com>
2025-11-30 22:16:16 -0500

    Update file store to include swapPairs

--- apps/web/src/utils/zustand/file-store.ts
+++ apps/web/src/utils/zustand/file-store.ts
@@ -1,17 +1,22 @@
 import { create } from 'zustand';
 import { FONTS } from '../constants';
 
+export type SwapPair = [string, string];
+
 type FileStore = {
   content: string;
   font: string;
   letterSpacing: number;
   backgroundColor: string;
   maintextColor: string;
+  swapPairs: SwapPair[];
   setContent: (newContent: string) => void;
   setFont: (newFont: string) => void;
   setLetterSpacing: (newSpacing: number) => void;
   setBackgroundColor: (newColor: string) => void;
   setMainTextColor: (newTextColor: string) => void;
+  addSwapPair: (pair: SwapPair) => void;
+  removeSwapPair: (index: number) => void;
 };
 
 export const useFileStore = create<FileStore>((set) => ({
@@ -20,6 +25,9 @@ export const useFileStore = create<FileStore>((set) => ({
   letterSpacing: 0,
   backgroundColor: '#FFFFFF',
   maintextColor: 'black',
+
+  swapPairs: [],
+
   setContent: (newContent: string) => {
     set({ content: newContent });
   },
@@ -35,4 +43,10 @@ export const useFileStore = create<FileStore>((set) => ({
   setMainTextColor: (newTextColor: string) => {
     set({ maintextColor: newTextColor });
   },
+  addSwapPair: (pair: SwapPair) => set((state: FileStore) => ({ 
+    swapPairs: [...state.swapPairs, pair] 
+  })),
+  removeSwapPair: (index: number) => set((state: FileStore) => ({ 
+    swapPairs: state.swapPairs.filter((_, i) => i !== index) 
+  })),
 }));
