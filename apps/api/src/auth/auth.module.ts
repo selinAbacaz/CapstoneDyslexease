95ba5e65dec718507e7c61e627842276b7b2236e
TheLuckyman30 <bnauta0@gmail.com>
2025-12-04 12:23:37 -0500

    Setup initial auth module

---
+++ apps/api/src/auth/auth.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { JwtModule } from '@nestjs/jwt';
+import { PrismaService } from 'src/prisma.service';
+
+@Module({
+  imports: [JwtModule.register({})],
+  controllers: [AuthController],
+  providers: [AuthService, PrismaService],
+})
+export class AuthModule {}
