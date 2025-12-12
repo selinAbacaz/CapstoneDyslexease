eeee1b5a404de3703e686b6e310e7f401d7012ff
TheLuckyman30 <bnauta0@gmail.com>
2025-12-12 04:37:00 -0500

    Store now resets zustand state

--- apps/api/src/files/files.service.ts
+++ apps/api/src/files/files.service.ts
@@ -195,7 +195,7 @@ export class FilesService {
   ): Promise<FileOutWithPrefs> {
     const { file_cuid } = deleteFileDto;
     try {
-      return this.prisma.file.delete({
+      const deletedFile = await this.prisma.file.delete({
         where: { file_cuid, user_cuid },
         select: {
           file_cuid: true,
@@ -221,6 +221,13 @@ export class FilesService {
           },
         },
       });
+
+      await this.prisma.user.update({
+        where: { user_cuid },
+        data: { selected_file_cuid: null },
+      });
+
+      return deletedFile;
     } catch (error) {
       if (
         error instanceof Prisma.PrismaClientKnownRequestError &&
--- apps/web/src/components/Delete.tsx
+++ apps/web/src/components/Delete.tsx
@@ -7,22 +7,29 @@ import { fetcher } from '../utils/fetching';
 export function Delete() {
   const qc = useQueryClient();
   const selectedFileId = useFileStore((state) => state.selectedFileId);
+  const reset = useFileStore((state) => state.reset);
   const deleteMutation = useMutation({
     mutationFn: (deleteFile: DeleteFile): Promise<FileOut> =>
       fetcher<FileOut>({
         endpoint: '/files',
         init: { method: 'DELETE', body: JSON.stringify(deleteFile) },
       }),
-    onSuccess: () => {
-      qc.invalidateQueries({ queryKey: ['files'] });
+    onSuccess: async () => {
+      await Promise.all([
+        qc.invalidateQueries({ queryKey: ['files'] }),
+        qc.invalidateQueries({ queryKey: ['file', selectedFileId] }),
+      ]);
     },
   });
 
+  function handleDelete() {
+    const oldId = selectedFileId;
+    reset();
+    deleteMutation.mutate({ file_cuid: oldId });
+  }
+
   return (
-    <Button
-      hidden={!selectedFileId}
-      onClick={() => deleteMutation.mutate({ file_cuid: selectedFileId })}
-    >
+    <Button hidden={!selectedFileId} onClick={handleDelete}>
       Delete File
     </Button>
   );
